2e43f2248ec82575096ffc4ef5a683552277743e
Josh Wood <76712541+Kalaghni@users.noreply.github.com>
2022-07-12 20:36:17 -0400

    Hydrogen / Basic App

---
+++ react-playground/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ResponsiveAppBar from './Navbar';
+import MediaCard from './Card';
+
+function App() {
+  return (
+    <div>
+    <ResponsiveAppBar />
+            <MediaCard />
+  </div>
+  );
+}
+
+export default App;
